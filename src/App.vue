eb2284d90a508de862dc259501bd6ec41942405c
Dapo Michaels <dapomichaels@gmail.com>
2021-02-07 14:48:27 +0100

    complete initial setup of router and vuex

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="app">
+		<router-view/>
+	</div>
+</template>
+
+<style>
+
+body, html {
+  height: 100%;
+}	
+#app {
+	height: 100%
+}
+.container {
+  @apply mx-auto;
+}
+
+</style>
